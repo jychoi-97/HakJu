9da36b59a734cdf929ffe1b40fc3c25b14841ca6
mandydayfly <20171998@sungshin.ac.kr>
2019-07-24 19:29:47 +0900

    메뉴 추가

--- app/src/main/java/com/example/hakju/MainActivity.java
+++ app/src/main/java/com/example/hakju/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.hakju;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.toCart);
+        button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(), cart.class);
+                startActivity(intent);
+            }
+        });
+
+        Button button1 = (Button) findViewById(R.id.mn);
+        button1.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                Intent int1 = new Intent(getApplicationContext(), menu.class);
+                startActivity(int1);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/hakju/cart.java
+++ app/src/main/java/com/example/hakju/cart.java
@@ -2,12 +2,18 @@ package com.example.hakju;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class cart extends AppCompatActivity {
+public class cart extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
