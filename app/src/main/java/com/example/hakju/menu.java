bbef91d4c6ead95e8d917e78ede979ae8a4f41f2
mandydayfly <20171998@sungshin.ac.kr>
2019-07-29 00:35:15 +0900

    메뉴 추가2

--- app/src/main/java/com/example/hakju/menu.java
+++ app/src/main/java/com/example/hakju/menu.java
@@ -1,8 +1,17 @@
 package com.example.hakju;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.NumberPicker;
+import android.widget.SlidingDrawer;
+import android.widget.Spinner;
 import android.widget.TabHost;
+
 
 public class menu extends AppCompatActivity {
 
@@ -14,11 +23,13 @@ public class menu extends AppCompatActivity {
         TabHost tabHost1 = (TabHost) findViewById(R.id.tabHost1) ;
         tabHost1.setup() ;
 
+
         // 첫 번째 Tab. (탭 표시 텍스트:"TAB 1"), (페이지 뷰:"content1")
         TabHost.TabSpec ts1 = tabHost1.newTabSpec("Tab Spec 1") ;
         ts1.setContent(R.id.content1) ;
         ts1.setIndicator("Roll&Noodle") ;
         tabHost1.addTab(ts1)  ;
+
 
         // 두 번째 Tab. (탭 표시 텍스트:"TAB 2"), (페이지 뷰:"content2")
         TabHost.TabSpec ts2 = tabHost1.newTabSpec("Tab Spec 2") ;
@@ -32,5 +43,47 @@ public class menu extends AppCompatActivity {
         ts3.setIndicator("Fry&Rice") ;
         tabHost1.addTab(ts3) ;
 
+        Spinner spinnerMenu1 = (Spinner)findViewById(R.id.spinnerMenu1);
+        spinnerMenu1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        NumberPicker menu1num = (NumberPicker)findViewById(R.id.menu1num);
+        menu1num.setMaxValue(20);
+        menu1num.setMinValue(1);
+        menu1num.setValue(1);
+
+        Button btnCart = (Button)findViewById(R.id.btnCart);
+
+        btnCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), cart.class);
+                startActivity(intent);
+            }
+        });
+
+        Button btnOrder = (Button)findViewById(R.id.btnOrder);
+
+        btnOrder.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), OrderActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
+
 }
+
+
