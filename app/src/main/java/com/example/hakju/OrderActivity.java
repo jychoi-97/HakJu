58eadba3c814c4cc0f85c15b0118c11a6734d102
choijiyune <fedraijf@gmail.com>
2019-07-29 00:53:59 +0900

    파이어베이스 연동 및 주문내역 리싸이클러뷰 아이템 생성

--- app/src/main/java/com/example/hakju/OrderActivity.java
+++ app/src/main/java/com/example/hakju/OrderActivity.java
@@ -1,13 +1,38 @@
 package com.example.hakju;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 
 public class OrderActivity extends AppCompatActivity {
 
+    public static class OrderViewHolder extends RecyclerView.ViewHolder{
+
+        ImageView circle;
+        TextView foodName;
+        TextView foodNumber;
+        TextView foodNumberGae;
+
+        public OrderViewHolder(@NonNull View itemView) {
+            super(itemView);
+            circle = itemView.findViewById(R.id.circleCircle);
+            foodName = itemView.findViewById(R.id.orderFoodName);
+            foodNumber = itemView.findViewById(R.id.orderFoodNumber);
+            foodNumberGae = itemView.findViewById(R.id.orderFoodGae);
+        }
+    }
+
+    private RecyclerView oOrderRecyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+
+        oOrderRecyclerView = findViewById(R.id.recycler1);
     }
 }
