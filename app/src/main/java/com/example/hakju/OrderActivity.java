4c0a721729a752357c3f9ed223102744b6e79f6e
choijiyune <fedraijf@gmail.com>
2019-07-30 01:05:36 +0900

    결제창 리싸이클러뷰 생성

--- app/src/main/java/com/example/hakju/OrderActivity.java
+++ app/src/main/java/com/example/hakju/OrderActivity.java
@@ -3,36 +3,32 @@ package com.example.hakju;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class OrderActivity extends AppCompatActivity {
 
-    public static class OrderViewHolder extends RecyclerView.ViewHolder{
-
-        ImageView circle;
-        TextView foodName;
-        TextView foodNumber;
-        TextView foodNumberGae;
-
-        public OrderViewHolder(@NonNull View itemView) {
-            super(itemView);
-            circle = itemView.findViewById(R.id.circleCircle);
-            foodName = itemView.findViewById(R.id.orderFoodName);
-            foodNumber = itemView.findViewById(R.id.orderFoodNumber);
-            foodNumberGae = itemView.findViewById(R.id.orderFoodGae);
-        }
-    }
-
-    private RecyclerView oOrderRecyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
 
-        oOrderRecyclerView = findViewById(R.id.recycler1);
+        RecyclerView orderRecyclerView = findViewById(R.id.recycler1);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        orderRecyclerView.setLayoutManager(layoutManager);
+
+        List<OrderItem> orderDataList = new ArrayList<>();
+        for(int i=0; i<10; i++){
+            orderDataList.add(new OrderItem(i+"음식", i));
+        }
+        OrderRecyclerAdapter adapter = new OrderRecyclerAdapter(orderDataList);
+        orderRecyclerView.setAdapter(adapter);
     }
 }
