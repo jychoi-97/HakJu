66686c23cd9f6979447b52be32203534db995306
mandydayfly <20171998@sungshin.ac.kr>
2019-08-01 02:14:25 +0900

    메뉴 추가3

---
+++ app/src/main/java/com/example/hakju/MenuData.java
@@ -0,0 +1,16 @@
+package com.example.hakju;
+
+public class MenuData {
+    public String productName;
+    public int productNum;
+
+    public MenuData(){
+
+    }
+
+    public MenuData(String productName, int productNum){
+        this.productName = productName;
+        this.productNum = productNum;
+    }
+
+}
