4353644f0f372670d25118089ff89bd18211039c
13ZD970-GX55K <20171992@sungshin.ac.kr>
2019-08-12 21:28:12 +0900

    클라우드 메시지

--- app/src/main/java/com/example/hakju/MainActivity.java
+++ app/src/main/java/com/example/hakju/MainActivity.java
@@ -4,16 +4,24 @@ package com.example.hakju;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+
 public class MainActivity extends AppCompatActivity {
 
     EditText editText;
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +47,21 @@ public class MainActivity extends AppCompatActivity {
 
             }
 
+        });
+
+
+        FirebaseInstanceId.getInstance().getInstanceId().addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+            @Override
+            public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                if(!task.isSuccessful()){
+                    Log.w(TAG,"getInstanceId failed", task.getException());
+                    return;
+                }
+                String token = task.getResult().getToken();
+
+                String msg = getString(R.string.msg_token_fmt, token);
+                Log.d(TAG,msg);
+            }
         });
 
     }
