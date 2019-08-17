91f7540ea663d2374e70fa611d9b38cc1bf2ca1a
13ZD970-GX55K <20171992@sungshin.ac.kr>
2019-08-17 17:37:53 +0900

    메뉴 추가

--- app/src/main/java/com/example/hakju/FragmentRoll.java
+++ app/src/main/java/com/example/hakju/FragmentRoll.java
@@ -39,6 +39,9 @@ public class FragmentRoll extends Fragment {
         lstFood.add(new Food("야채김밥\n1500원", "Categorie Food", "Description Food", R.drawable.kimbap));
         lstFood.add(new Food("라면\n2300원", "Categorie Food", "Description Food", R.drawable.ramyeon));
         lstFood.add(new Food("불고기\n4800원", "Categorie Food", "Description Food", R.drawable.bulgogi));
+        lstFood.add(new Food("물냉면\n2800원", "Categorie Food", "Description Food", R.drawable.mulnang));
+        lstFood.add(new Food("돈육순두부찌개\n4000원", "Categorie Food", "Description Food", R.drawable.softtofu));
+        lstFood.add(new Food("돈까스쟁반국수\n4300원", "Categorie Food", "Description Food", R.drawable.donjang));
     }
 }
 
